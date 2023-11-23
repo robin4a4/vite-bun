235fd5239098f080b9c20d75001d0bfb0373c6fc
robin marillia <robin4a4@gmail.com>
2023-11-23 23:14:22 +0100

    add suspense

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,6 @@
-export async function App() {
+import { Suspense } from "react";
+
+async function Todos() {
   const todos = await fetch("https://jsonplaceholder.typicode.com/todos").then(
     (res) => res.json()
   );
@@ -7,6 +9,17 @@ export async function App() {
       {todos.map((todo) => (
         <div>{todo.title}</div>
       ))}
+    </div>
+  );
+}
+
+export async function App() {
+  return (
+    <div>
+      <h1>My todos</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Todos />
+      </Suspense>
     </div>
   );
 }
